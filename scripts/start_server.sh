1f41d32b790033f8d016c33224c270c60eb82546
osawa-koki <koki@osawa.dev>
2024-08-10 23:30:13 +0900

    small fix

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/ec2-user/ec2-user
+cd /home/ec2-user/app
 pip3 install -r requirements.txt
-nohup uvicorn main:app --host 0.0.0.0 --port 80 &
+nohup uvicorn app.main:app --host 0.0.0.0 --port 8000 &
