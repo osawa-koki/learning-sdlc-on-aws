b668e2d37aaa017114f824c352934887214a661f
osawa-koki <koki@osawa.dev>
2024-08-10 23:44:15 +0900

    デバグ用の出力。

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "cd to app directory" >> /home/ec2-user/server.log
 cd /home/ec2-user/app
+
+echo "installing requirements" >> /home/ec2-user/server.log
 pip3 install -r requirements.txt
+
+echo "starting server" >> /home/ec2-user/server.log
 nohup uvicorn app.main:app --host 0.0.0.0 --port 8000 &
